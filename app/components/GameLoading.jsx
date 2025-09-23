6d7eec014de8ffa69830376aa528c36e39b46669
Hasan-Itani <itaniHasan64@gmail.com>
2025-09-23 17:05:20 +0300

    slotBoard added

--- app/components/GameLoading.jsx
+++ app/components/GameLoading.jsx
@@ -25,26 +25,26 @@ export default function GameLoading({ durationMs = 3500, onComplete }) {
   }, [durationMs, onComplete]);
 
   return (
-    <div
-      className="w-screen h-screen flex items-center justify-center bg-center bg-no-repeat bg-cover"
+     <div
+      className="w-screen h-screen flex items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: `url('/ui/loading.png')` }}
     >
-      <div className="relative w-11/12 max-w-4xl">
-        <img src="/ui/progressbar.png" alt="frame" className="absolute top-0 left-0 w-full z-10" />
-        <img src="/ui/progress_hollow.png" alt="hollow" className="absolute top-12 left-4 w-[86%] z-20" />
+      <div className="relative w-3/4 max-w-4xl">
+        <img src="/ui/progressbar.png" alt="progress frame" className="absolute top-0 left-0 w-full z-10" />
 
-        <div
-          className="absolute top-4 left-4 h-[100px] z-30 overflow-hidden"
-          style={{ width: `calc(${progress}% + 10%)` }}
-        >
-          <img src="/ui/progress_active.png" alt="active" className="w-[85%] h-full object-cover" />
+        <img src="/ui/progress_hollow.png" alt="progress hollow" className="absolute top-12 left-15 w-[86%] z-20" />
+
+        <div className="absolute left-15 top-4 h-[100px] w-[90%] overflow-hidden z-20" style={{ width: `${progress +10}%` }}>
+          <img src="/ui/progress_active.png" alt="progress active" className="w-[78%]  h-full object-cover" />
+          
         </div>
 
         <div
-          className="absolute top-8 h-[100px] flex items-center z-40"
-          style={{ left: `calc(${progress}% + 10%)`, transform: "translateX(-50%)" }}
+          className="absolute ml-30 top-4 h-[100px] items-center z-40"
+          style={{ left: `${progress * 0.79}%`, transform: "translateX(-50%)" }}
         >
-          <img src="/ui/progress_fire.png" alt="flame" className="h-16" />
+          <img src="/ui/progress_fire.png" alt="progress fire" className="relative h-[100px]" />
+          
         </div>
       </div>
     </div>
